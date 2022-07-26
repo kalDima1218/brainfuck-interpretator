0fe0a566c5770b6e721a28fc29dc00c295f53fea
Dmitry Kalugin <kalugin180806@gmail.com>
2022-07-26 20:53:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,23 +10,35 @@
 
 int main() {
     char memory[30000] = {};
-    std::string script = "++++++++++++++++++++++++++++++++++++++++++++++++[>+<-]>.";
-    char *p = &memory[0];
+    std::string script = "+++++>>+>+>+>++++++++>+>+>+>++++++>+>+>+>+++>+>+>+>+++++++++>+>+>+>+>+>+>+><[<]<[->>>>>>>>>[>->->-<<<<<<<<-<<->>>[>>>>>+>+<<<<<<-]>>>>>[<<<<<+>>>>>-]<[>+>>+<<<-]>[<+>-]<<<<<[->>>>-[>]<<<<<]<[->>[-]>>>>>>[<<+>>-]>[<<<<<<<+>>>>>>>-]<<<<<<<<<]<[->>>>>>[-]>>[<<<<<<+>>>>>>-]>[<<<+>>>-]<<<<<<<<<<]+>+>+>>>>>>+>+>+<<<<<<<>>>>>>>>]<<<<[<]<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.>>>>]";
     int i = 0;
     std::stack<int> while_open;
+    int *next = new int[script.size()], *prev = new int[script.size()];
     while(i != script.size()){
-        if(script[i] == '+') (*p)++;
-        else if(script[i] == '-') (*p)--;
-        else if(script[i] == '>') p++;
-        else if(script[i] == '<') p--;
-        else if(script[i] == '['){
-            if(*p == 0) while(script[++i] != ']');
-            else while_open.push(i);
+        next[i] = i+1;
+        prev[i] = i-1;
+        if(script[i] == '['){
+            while_open.push(i);
         }
         else if(script[i] == ']'){
-            i = while_open.top() - 1;
+            int p = while_open.top();
             while_open.pop();
+            prev[i] = p;
+            next[p] = i;
         }
+        i++;
+    }
+    i = 0;
+    char *p = &memory[0];
+    while(i != script.size()){
+        if(script[i] == '>') p++;
+        else if(script[i] == '<') p--;
+        else if(script[i] == '+') (*p)++;
+        else if(script[i] == '-') (*p)--;
+        else if(script[i] == '[') {
+            if(*p == 0) i = next[i];
+        }
+        else if(script[i] == ']') i = prev[i] - 1;
         else if(script[i] == '.') putchar(*p);
         else if(script[i] == ',') *p = getchar();
         i++;
